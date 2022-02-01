d0a5a5acdc5811add9bc9fd1bcc37cc1200bf5d4
bsitenga <bsitenga@gmail.com>
2022-01-31 19:27:48 -0800

    boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Dough to Bread
+      This is dough to Bread
+    </div>
+  );
+}
+
+export default App;
---
+++ server/routes/blogPosts.routes.js
@@ -0,0 +1,22 @@
+// routes/blogPosts.routes.js
+import express from "express";
+
+import {
+  getAllBlogPosts,
+  addBlogPost,
+  getSinglePost,
+  updateSingleBlogPost,
+  removeSingleBlogPost,
+  likeBlogPost,
+} from "../controllers/blogPosts.controller.js";
+
+const router = express.Router();
+
+router.get("/", getAllBlogPosts);
+router.post("/", addBlogPost);
+router.get("/:id", getSinglePost);
+router.patch("/:id", updateSingleBlogPost);
+router.delete("/:id", removeSingleBlogPost);
+router.patch("/:id/likeedBlogPost", likeBlogPost);
+
+export default router;
